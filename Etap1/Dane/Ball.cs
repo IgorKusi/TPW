9ce07adf2474573ce034cccb5fa42d8157d11918
IgorKusi <igor.kusidel@gmail.com>
2023-04-04 15:40:34 +0200

    Start of work on Ex.2

---
+++ Etap1/Dane/Ball.cs
@@ -0,0 +1,15 @@
+﻿namespace Dane
+{
+    public class Ball
+    {
+        public int X { get; }
+        public int Y { get; }
+        public void ChangePosition(int newX, int newY)
+        {
+            X = newX;
+            Y = newY;
+            BallChangedEvent.Raise(newX, newY);
+        }
+        public event BallChangedEvent;
+        }
+    }
---
+++ Etap1/Dane/Map.cs
@@ -0,0 +1,18 @@
+﻿using Dane;
+using System.Xml.Linq;
+
+namespace Logika {
+    public class Map {
+
+        public int Xsize { get; set; }
+        public int Ysize { get; set; }
+        public IList<Ball> Balls;
+
+        public Map(int xsize, int ysize) {
+            Xsize = xsize; 
+            Ysize = ysize;
+            Balls = new List<Ball>();
+            
+        }
+    }
+}
